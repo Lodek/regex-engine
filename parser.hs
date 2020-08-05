4b97ae3399b7adbf0daaa378da7857498781fe36
Lodek <23224552+Lodek@users.noreply.github.com>
2020-08-05 16:19:11 -0300

    token parsing

--- parser.hs
+++ parser.hs
@@ -1,13 +1,25 @@
-data Quantifier = Kleene | Optional
-data Operation = Concat | Alternation
 data Symbol = Char
-
-alphabet = "abcdefghjklmnopqrstuvwxyz*()|"
+data Token = Symbol | Kleene | Alternation | Concat | GroupBegin | GroupEnd
 
 data ParseTree = Node ParseTree Operation ParseTree | Node Parsetree Quantifier | Symbol
+
+symbols = "abcdefghjklmnopqrstuvwxyz"
+alphabet = "()*|" ++ symbols
 
 -- |Verify that all characters in the input string are part of the defined alphabet
 isInAlphabet :: String -> Boolean
 isInAlphabet = all $ map (\token -> token `elem` alphabet)
 
 regexToTree :: String -> ParseTree
+
+genTokens :: String -> [Token]
+genTokens  = map genToken
+
+genToken :: Symbol -> Token
+genToken '(' = GroupBegin
+	 ')' = GroupEnd
+	 '|' = Alternation
+	 '*' = Kleene
+	 c = if c `elem` symbols then c else '!'
+
+genTree :: [Token] -> ParseTree
