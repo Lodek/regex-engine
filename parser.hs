a6d95a60ae7acd33c034ec0b0c37f7774b3d0f8f
Lodek <23224552+Lodek@users.noreply.github.com>
2020-08-10 15:39:23 -0300

    fixing data types

--- parser.hs
+++ parser.hs
@@ -1,7 +1,8 @@
-data Symbol = Char
-data Token = Symbol | Kleene | Alternation | Concat | GroupBegin | GroupEnd
-
-data ParseTree = Node ParseTree Operation ParseTree | Node Parsetree Quantifier | Symbol
+type Symbol = Char 
+data Operator = Concat | Alternation
+data Quantifier = Kleene
+data Token = Symbol | Quantifier | Operator | GroupBegin | GroupEnd
+data ParseTree = Node ParseTree Operator ParseTree | QuantifierNode Symbol Quantifier | Singleton Symbol
 
 symbols = "abcdefghjklmnopqrstuvwxyz"
 alphabet = "()*|" ++ symbols
@@ -9,8 +10,6 @@ alphabet = "()*|" ++ symbols
 -- |Verify that all characters in the input string are part of the defined alphabet
 isInAlphabet :: String -> Boolean
 isInAlphabet = all $ map (\token -> token `elem` alphabet)
-
-regexToTree :: String -> ParseTree
 
 genTokens :: String -> [Token]
 genTokens  = map genToken
@@ -22,7 +21,6 @@ genToken '(' = GroupBegin
 	 '*' = Kleene
 	 c = if c `elem` symbols then c else '!'
 
-genTree :: [Token] -> ParseTree
 
 -- |Adds explicit Concat tokens in between symbols or groups
 normalizeStream :: [Token] -> [Token]
@@ -32,3 +30,15 @@ normalizeStream x1:x2:xs = case (x1, x2) of
 				(Symbol, Symbol) -> x1:Concat:x2: normalizeStream xs
 				(Symbol, GroupBegin) -> x1:Concat:x2: normalizeStream xs
 				(_, _) -> x1:x2: normalizeStream xs
+
+
+treeBuilder :: [Token] -> (ParseTree, [Token])
+
+
+seekTrees :: [Token] -> (ParseTree, [Token])
+seekTrees x1:[] = x1
+seekTrees x1:xs = case x1 of
+                       Group_Begin -> treeBuilder xs
+seekTrees x1:x2:xs = case (x1, x2) of
+                          (Symbol, Quantifier) -> Node Symbol Quantifier
+                          (Symbol, Operator) -> Node Symbol
