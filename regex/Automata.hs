b9644aba74abb31822083758cbd7a0af6e5eb0d6
Lodek <23224552+Lodek@users.noreply.github.com>
2020-08-31 14:04:33 -0300

    added function signatures

--- regex/Automata.hs
+++ regex/Automata.hs
@@ -1,39 +1,59 @@
+module Automata where 
+
 import qualified Data.Set as Set
+import Data.Either
 
-type Delta s t = (s -> AlphabetSymbol t -> [s])
+data SigmaElem s = Symbol s | Epsilon deriving (Show, Eq)
 
-data AlphabetSymbol t = Symbol t | Epsilon
+type Sigma s = Set.Set (SigmaElem s)
 
-data Automata t s = NFA { alphabet :: Set AlphabetSymbol t,
-                          states :: Set s,
-                          q0 :: s,
-                          qa :: Set s,
-                          delta :: (s -> t -> Set s)
-                        } Deriving (Show)
+type Delta q s = (q -> SigmaElem s -> [q])
+
+data Automata q s = NFA { alphabet :: Set.Set (SigmaElem s),
+                          states :: Set.Set q,
+                          q0 :: q,
+                          qa :: Set.Set q,
+                          delta :: Delta q s
+                        } 
 
 
--- |Build alphabet with Epsilon from list of symbols
-buildEpsilonAlphabet :: [t] -> Set AlphabetSymbol t
-buildEpsilonAlphabet ts = Set.fromList $ Epsilon:(map Symbol ts)
+-- |Wrap foldable of symbols as a list of SigmaElems, adding epsilon
+buildSigma :: (Foldable t, Ord a) => t a -> Set.Set (SigmaElem a)
+buildSigma ts = Set.insert Epsilon symbols
+    where empty = Set.empty :: Set.Set (SigmaElem a)
+          symbols = foldl (\acc s -> Set.insert (Symbol s) acc) empty ts
+
+-- |If state is in return true if state is accepting else false
+--isAccepting :: [q] -> q -> Bool
+--isAccepting qs q = q elem qs
+
 
 
 -- |Return list of states reachable from given states with epsilon transitions alone
-epsilonClosure :: Delta s t -> [s] -> [s]
-epsilonClosure delta [] = []
-epsilonClosure delta ss = let ss' = concat $ map (\s -> delta s Epsilon) ss in
-                              ss' ++ epsilonClosure ss'
+--epsilonClosure :: Delta s t -> [s] -> [s]
+--epsilonClosure delta [] = []
+--epsilonClosure delta ss = let ss' = concat $ map (\s -> delta s Epsilon) ss in
+                              --ss' ++ epsilonClosure delta ss'
 
 
-stateEpsilonClosure :: Delta s t -> s -> [s]
-stateEpsilonClosure delta s = epsilonClosure delta [s]
+--stateEpsilonClosure :: Delta s t -> s -> [s]
+--stateEpsilonClosure delta s = epsilonClosure delta [s]
 
 
-move :: Delta s t -> s -> AlphabetSymbol t -> [s]
-move delta st sy = let ss = delta st sy in
-                       ss:(epsilonClosure delta ss)
+--move :: Delta s t -> s -> SigmaElem t -> [s]
+--move delta st sy = let ss = delta st sy in
+                       --ss ++ epsilonClosure delta ss
 
 
-moveStates :: Delta s t -> [s] -> AlphabetSymbol t -> [s]
-moveStates delta ss sy = concat $ map (\s -> move delta s sy) ss
+--moveStates :: Delta s t -> [s] -> SigmaElem t -> [s]
+--moveStates delta ss sy = concat $ map (\s -> move delta s sy) ss
 
---execNFA :: Automata t s -> [t] -> Bool
+
+-- |Validate elements of stream are in Sigma, return either with error message or stream
+--streamInSigma :: Sigma s -> [s] -> Either [String] [s]
+
+--eval :: Automata q s -> [s] -> Either String Bool
+--simple functions and  delegate error handling
+--have wrapper functions to operate over automata
+--dumb implementation, no partial matching; valid only whole stream is valid.
+--public interface for building automata and state transition function
