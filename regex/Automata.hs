11c57ebd9449efe1698374bb7e18a465645b3588
Lodek <23224552+Lodek@users.noreply.github.com>
2020-09-16 11:00:53 -0300

    refactored automata states to be int, instead of parametric

--- regex/Automata.hs
+++ regex/Automata.hs
@@ -5,6 +5,8 @@ import Data.Either
 import Data.Tuple
 
 data SigmaElem s = Symbol s | Epsilon deriving (Show, Eq)
+type Alphabet s = Set.Set (SigmaElem s)
+type State = Int
 
 instance (Ord s) => Ord (SigmaElem s) where --unecessary, just for learning
     Epsilon < _ = True
@@ -16,14 +18,14 @@ instance (Ord s) => Ord (SigmaElem s) where --unecessary, just for learning
 
 type Sigma s = Set.Set (SigmaElem s)
 
-type Delta q s = (q -> SigmaElem s -> Set.Set q)
+type Delta s = (State -> SigmaElem s -> Set.Set State)
 
---data Automata q s = NFA { alphabet :: Set.Set (SigmaElem s),
-                          --states :: Set.Set q,
-                          --q0 :: q,
-                          --qas :: Set.Set q,
-                          --delta :: Delta q s
-                        --} 
+data Automata s = NFA { alphabet :: Set.Set (SigmaElem s),
+                          states :: Set.Set State,
+                          q0 :: State,
+                          qas :: Set.Set State,
+                          delta :: Delta s
+                        } 
 
 
 -- |Wrap foldable of symbols as a list of SigmaElems, adding epsilon
@@ -38,33 +40,33 @@ unionAndApplyOverDiff :: (Ord a) => (Set.Set a -> Set.Set a) -> Set.Set a -> Set
 unionAndApplyOverDiff f a b = (Set.union a b, f (Set.difference a b))
 
 -- | Apply delta over Set of states for a symbol, return the union of all reachable states
-deltaOverSet :: (Ord q) => Delta q s -> SigmaElem s -> Set.Set q -> Set.Set q
+deltaOverSet :: Delta s -> SigmaElem s -> Set.Set State -> Set.Set State
 deltaOverSet delta s qs = foldl (\acc q -> Set.union (delta q s) acc) Set.empty qs
 
 
 -- |Return list of states reachable from given states with epsilon transitions alone
-epsilonClosure :: (Ord q) => Delta q s -> Set.Set q -> Set.Set q -> (Set.Set q, Set.Set q)
+epsilonClosure :: Delta s -> Set.Set State -> Set.Set State -> (Set.Set State, Set.Set State)
 epsilonClosure delta states visited
      | Set.null states = (states, visited)
      | otherwise = uncurry (epsilonClosure delta) $ swap $ unionAndApplyOverDiff delta' states visited
      where delta' = deltaOverSet delta Epsilon
 
 
-epsilonClosure' :: (Ord q) => Delta q s -> Set.Set q -> Set.Set q
+epsilonClosure' :: Delta s -> Set.Set State -> Set.Set State
 epsilonClosure' delta qs = snd $ epsilonClosure delta qs Set.empty
 
 -- |If state is in return true if state is accepting else false
 -- isAccepting :: Set.Set q -> q -> Bool
 
-stateEpsilonClosure :: (Ord q) => Delta q s -> q -> Set.Set q
+stateEpsilonClosure :: Delta s -> State -> Set.Set State
 stateEpsilonClosure delta q = epsilonClosure' delta $ Set.singleton q
 
 
-move :: (Ord q) => Delta q s -> q -> SigmaElem s -> Set.Set q
+move :: Delta s -> State -> SigmaElem s -> Set.Set State
 move delta q s = deltaOverSet delta s $ epsilonClosure' delta $ Set.singleton q
 
 
-moveStates :: (Ord q) => Delta q s -> Set.Set q -> SigmaElem s -> Set.Set q
+moveStates :: Delta s -> Set.Set State -> SigmaElem s -> Set.Set State
 moveStates delta qs s = foldl (\acc s -> Set.union s acc) Set.empty states
     where states = map (\q -> move delta q s) $ Set.toList qs
 
@@ -72,8 +74,8 @@ moveStates delta qs s = foldl (\acc s -> Set.union s acc) Set.empty states
 -- |Validate elements of stream are in Sigma, return either with error message or stream
 -- streamInSigma :: Sigma s -> [s] -> Either [String] [s]
 
-eval :: (Ord q, Ord s) => Delta q s -> q -> Set.Set q -> [s] -> Bool
-eval delta q0 qas ss = isAccepting $ foldl ffunction (Set.singleton q0) ss
+eval :: (Ord s) => Delta s -> State -> Set.Set State -> [s] -> Bool
+eval delta q0 qas ss = isAccepting $ epsilonClosure' delta $ foldl ffunction (Set.singleton q0) ss
     where isAccepting qs = qs `Set.isSubsetOf` qas
           ffunction qs s = moveStates delta qs (Symbol s)
 
